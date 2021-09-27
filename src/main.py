66849c346654c20d81f59ac4c18c53fa03cdb0d5
s-nao <s.sugar.nj@gmail.com>
2021-09-27 22:59:49 +0900

    [wip]100mb以下の形でテストデータを追加し簡単に味方のアイコン把握まで

--- src/main.py
+++ src/main.py
@@ -4,6 +4,16 @@ import cv2
 # カメラ等でストリーム再生の場合は引数に0等のデバイスIDを記述する
 video = cv2.VideoCapture('../resource/video/test_data.mp4')
 
+icon_width = 48
+icon_height = 56
+
+allies_positions = [
+    {"x": 360, "y": 19},
+    {"x": 415, "y": 19},
+    {"x": 474, "y": 19},
+    {"x": 531, "y": 19}
+]
+
 while video.isOpened():
     # フレームを読込み
     ret, frame = video.read()
@@ -15,12 +25,20 @@ while video.isOpened():
     # -----------------
     # 画像処理を記述する
     # -----------------
+    deframe = []
+
+    for position in allies_positions :
+        deframe.append(frame[position["y"]:position["y"]+icon_height, position["x"]:position["x"]+icon_height])
+        cv2.rectangle(frame, (position["x"], position["y"]),
+                      (position["x"]+icon_width, position["y"]+icon_height), (0, 0, 255), 1)
+
+    print(deframe)
 
     # フレームの描画
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', deframe[1])
 
     # qキーの押下で処理を中止
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1)
     if key == ord('q'):
         break
 
